567bd45e905356f771623cca64f2b0f7bbc30597
Bernardo Novaski <86263827+BernardoNovaski@users.noreply.github.com>
2021-07-21 11:24:51 -0300

    Primeiro Projeto Oficial do curso 3 de Java-Polimorfismo

---
+++ bytebank-herdado/src/TestaFuncionario.java
@@ -0,0 +1,16 @@
+
+public class TestaFuncionario {
+	
+	public static void main(String[] args) {
+		
+		Funcionario nico = new Funcionario();
+		nico.setNome("Nico Steppat");
+		nico.setCpf("222333444-5");
+		nico.setSalario(2500.00);
+		
+		System.out.println(nico.getNome());
+		System.out.println(nico.getBonificacao());
+		
+	}
+
+}
